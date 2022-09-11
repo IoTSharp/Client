8543fc5206176412015903ec4fc273982880753d
geffzhang <geffzhang@qq.com>
2022-09-11 22:40:32 +0800

    feat: 初始化项目
    1、 maui + blazor
    2、采用masa blazor 组件库

---
+++ IoTSharp.Maui/IoTSharp.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IoTSharp.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ IoTSharp.Maui/IoTSharp.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IoTSharp.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
