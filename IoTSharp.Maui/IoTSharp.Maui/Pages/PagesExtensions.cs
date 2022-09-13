8fe989777883c1bd33926e8159a88d1a47d75b3d
geffzhang <geffzhang@qq.com>
2022-09-13 17:58:04 +0800

    feat： remove unused code

--- IoTSharp.Maui/IoTSharp.Maui/Pages/PagesExtensions.cs
+++ IoTSharp.Maui/IoTSharp.Maui/Pages/PagesExtensions.cs
@@ -11,18 +11,9 @@ namespace IoTSharp.Maui.Pages
         public static MauiAppBuilder ConfigurePages(this MauiAppBuilder builder)
         {
             // main tabs of the app
-            //builder.Services.AddSingleton<DiscoverPage>();
-            //builder.Services.AddSingleton<SubscriptionsPage>();
-            //builder.Services.AddSingleton<ListenLaterPage>();
-            //builder.Services.AddSingleton<ListenTogetherPage>();
+            builder.Services.AddSingleton<HomePage>(); 
             builder.Services.AddSingleton<SettingsPage>();
-
-            // pages that are navigated to
-            //builder.Services.AddTransient<CategoriesPage>();
-            //builder.Services.AddTransient<CategoryPage>();
-            //builder.Services.AddTransient<EpisodeDetailPage>();
-            //builder.Services.AddTransient<ShowDetailPage>();
-
+ 
             return builder;
         }
     }
--- IoTSharp.Maui/IoTSharp.Maui/ViewModels/ViewModelExtensions.cs
+++ IoTSharp.Maui/IoTSharp.Maui/ViewModels/ViewModelExtensions.cs
@@ -4,18 +4,10 @@
     {
         public static MauiAppBuilder ConfigureViewModels(this MauiAppBuilder builder)
         {
-            //builder.Services.AddSingleton<CategoriesViewModel>();
-            //builder.Services.AddTransient<CategoryViewModel>();
-            //builder.Services.AddSingleton<DiscoverViewModel>();
-            //builder.Services.AddTransient<EpisodeDetailViewModel>();
-            //builder.Services.AddSingleton<EpisodeViewModel>();
-            //builder.Services.AddSingleton<ListenLaterViewModel>();
+ 
             builder.Services.AddSingleton<SettingsViewModel>();
             builder.Services.AddSingleton<ShellViewModel>();
-            //builder.Services.AddTransient<ShowDetailViewModel>();
-            //builder.Services.AddSingleton<ShowViewModel>();
-            //builder.Services.AddSingleton<SubscriptionsViewModel>();
-
+ 
             return builder;
         }
     }
